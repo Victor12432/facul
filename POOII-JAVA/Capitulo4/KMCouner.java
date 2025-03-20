b7f496d16e934b977ee14480712e30f139a9d0b2
Victor <victorhugo30705@gmail.com>
2025-03-20 11:20:36 -0300

    exercicios recomendados do capitulo 4 do livro do cancian

---
+++ POOII-JAVA/Capitulo4/KMCouner.java
@@ -0,0 +1,32 @@
+package Capitulo4;
+ /**
+  * KMCouner
+  */
+ public class KMCouner {
+    private Integer km, litro;
+
+    public void setKM(Integer km){
+        this.km = km;
+    }
+    public Integer getKM(){
+        return km;
+    }
+
+    public void setLitro(Integer litro){
+        this.litro = litro;
+    }
+    public Integer getLitro(){
+        return litro;
+    }
+
+    public double calculaKMPerLitro(Interger km, litro){
+        Boolena n = true
+        while (n = true){
+            if (km == 0 || litro == 0){
+                n = false;
+                break;
+            }
+        System.out
+        }
+    }
+ }
